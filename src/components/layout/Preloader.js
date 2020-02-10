aa8fc41970737a933805652fd4a23eea1f62df13
Me The Admin <webcontentyo@gmail.com>
2020-02-10 18:59:46 +0100

    Logs

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className='progress red light-4'>
+      <div className='indeterminate red'></div>
+    </div>
+  );
+};
+
+export default Preloader;
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
 
-const LogItem = () => {
+const Logs = () => {
   const [logs, setLogs] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -18,21 +20,21 @@ const LogItem = () => {
   };
 
   if (loading) {
-    return <h4>Loading</h4>;
+    return <Preloader />;
   }
 
   return (
-    <ul className='collection-with-header'>
+    <ul className='collection with-header'>
       <li className='collection-header'>
         <h4 className='center'>System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className='center'>No Logs to show</p>
       ) : (
-        logs.map(log => <li>{log.message}</li>)
+        logs.map(log => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );
 };
 
-export default LogItem;
+export default Logs;
